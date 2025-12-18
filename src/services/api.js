3496632278ae0a3cb7113a594aacc4e77ae495a5
abdelkarimmedtech <hamdiabdelkarim15@gmail.com>
2025-12-18 02:11:37 +0100

    dsfdsfs

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // Backend server URL
+  baseURL: "https://backend-medical-equipment-1.onrender.com/api", // Backend server URL
 });
 
 // 🔹 Automatically attach token to every request if logged in
